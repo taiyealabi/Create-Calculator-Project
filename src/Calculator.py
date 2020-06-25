996e2c5d10fb4ea620add2ddb440d9584b57f45e
taiyealabi <taiyealabi.10@gmail.com>
2020-06-24 23:50:34 -0400

    Feature: Setup calculator CsvReader.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,15 +1,20 @@
 def addition(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 class Calculator:
     result = 0
 
     def __init__(self):
-        x 2 + 2
-        self.result = X;
         pass
 
     def add(self, a, b):
-        self.result = a + b)
-        return addition(a, b)
+        self.result = addition(a + b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtract(a + b)
+        return self.result
 
