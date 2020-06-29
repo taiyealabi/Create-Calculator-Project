b88879a0cb3c36932ad9425982e10d7dc71068bc
taiyealabi <taiyealabi.10@gmail.com>
2020-06-28 23:21:32 -0400

    Fix: Setup calculatorTest.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -5,41 +5,42 @@ from CsvReader import CsvReader
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsinstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
         self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
         test_data = CsvReader('src/addition.csv').data
-        for role in test_data:
-            self.assertEqual(self.calculator.add(row['Value 1']), ((row['Value 2']), int(row['Result'])))
+        for row in test_data:
+            self.assertEqual(self.calculator.add((row['Value 1']), row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtract_method_calculator(self):
         test_data = CsvReader('src/subtraction.csv').data
-        for role in test_data:
+        for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1']), ((row['Value 2']), int(row['Result'])))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiply_method_calculator(self):
         test_data = CsvReader('src/multiplication.csv').data
-        for role in test_data:
+        for row in test_data:
             self.assertEqual(self.calculator.multiply(row['Value 1']), ((row['Value 2']), int(row['Result'])))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_divide_method_calculator(self):
         test_data = CsvReader('src/division.csv').data
-        for role in test_data:
+        for row in test_data:
             self.assertEqual(self.calculator.divide(row['Value 1']), ((row['Value 2']), int(row['Result'])))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_sq_method_calculator(self):
         test_data = CsvReader('src/square.csv').data
-        for role in test_data:
+        for row in test_data:
             self.assertEqual(self.calculator.sq(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
