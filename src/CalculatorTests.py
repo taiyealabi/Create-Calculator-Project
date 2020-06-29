d37bd1d90f027f6a131aec84e28998039363fece
taiyealabi <taiyealabi.10@gmail.com>
2020-06-28 23:37:16 -0400

    Fix: Updated CsvReader.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -26,6 +26,7 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 1']), ((row['Value 2']), int(row['Result'])))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+
     def test_multiply_method_calculator(self):
         test_data = CsvReader('src/multiplication.csv').data
         for row in test_data:
